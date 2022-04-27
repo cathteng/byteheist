2d87d0ab2394f907a2519c312055379cfd697057
mtrotter22 <78663424+mtrotter22@users.noreply.github.com>
2022-04-27 16:13:14 -0400

    bit moves

--- src/components/bit/Bit.js
+++ src/components/bit/Bit.js
@@ -4,10 +4,13 @@ import * as APP from "../../app.js";
 
 class Bit {
     constructor(position) {
+        this.counter = 0;
         this.position = position.clone();
         this.corrupted = false;
+        this.origY = position.clone().y;
 
-        const bitCollectGeo = new THREE.SphereGeometry(1);
+        // const bitCollectGeo = new THREE.SphereGeometry(1);
+        const bitCollectGeo = new THREE.BoxGeometry(2, 2, 2);
         const bitCollectMat = new THREE.MeshBasicMaterial({
             color: 0x0000ff,
             wireframe: true,
@@ -21,6 +24,22 @@ class Bit {
     handleCollisions(spherePosition) {
         let EPS = 3;
         if (this.corrupted) return;
+
+        // decrease BASE to spin faster
+        const BASE = 500;
+        const factor = 2 * Math.PI / BASE;
+        this.mesh.rotation.setFromVector3(new THREE.Vector3( 0, factor * this.counter, 0));
+
+        // moving up and down
+        const OSC_SPEED = 10; // higher is faster
+        let amp = Math.sin(this.counter * factor * OSC_SPEED);
+        const HEIGHT_CHANGE = 1;
+        let change = amp * HEIGHT_CHANGE;
+        // NOT this.position (do not change this.position pls)
+        this.mesh.position.y = this.origY + change;
+
+        this.counter = (this.counter+1) % BASE;
+
         let dist = Math.sqrt(
             (spherePosition.x - this.position.x) ** 2 +
             (spherePosition.y - this.position.y) ** 2 +
